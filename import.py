e22cc32787bbc153a029a8b94633c8d2216fe8b6
Nathan Shao <nathan.shao@hotmail.com>
2022-02-21 23:42:35 -0500

    First commit

---
+++ import.py
@@ -0,0 +1,20 @@
+from datetime import datetime, date
+from pytz import timezone
+
+tz = timezone('US/Eastern')
+now = datetime.now(tz).date()
+
+answer_list = {}
+
+def days_answer(today):
+	return answer_list[today]
+
+with open('answer_list.txt', 'r') as answers:
+	for line in answers:
+		line = line.strip()
+		line = line.split(' ')
+		date = datetime.strptime(line[0], "%Y-%m-%d")
+
+		answer_list[date.date()] = line[1]
+
+print(days_answer(datetime.now(tz).date()))
